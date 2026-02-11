63b886699f5dded84bdf9e3986facf1f7538d47a
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2026-02-11 08:36:39 -0500

    bump version number with reference to dev branch

--- quicknxs/version.py
+++ quicknxs/version.py
@@ -2,6 +2,6 @@
 
 # the minor version and change date gets automatically updated on a git commit
 version=(1, 1, 6)
-last_changes="2018-01-26 08:49:44"
+last_changes="2026-02-11 08:30:00"
 
-str_version=".".join(map(str, version))
+str_version=".".join(map(str, version))+" feature/pixi_py3_qt5"
